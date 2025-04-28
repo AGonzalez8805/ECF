789f9bebb6a4ad6e90b6106108f42417be1c01c7
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-04-28 16:25:30 +0200

    Autres pages

--- EcoRide/templates/footer.php
+++ EcoRide/templates/footer.php
@@ -1,9 +1,9 @@
 <footer class="text-stone-950 bg-stone-400 body-font">
-    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <p class="sm:ml-50 sm:pl-4 sm:py-2 sm:mt-0 mt-4"> contact@ecoride.fr</p>
-        <div class="sm:ml-120 sm:mt-0 mt-4 justify-start">
-            <p> Mentions Légales</p>
-        </div>
+    <div class="container px-5 py-8 mx-auto flex items-center md:flex-row flex-col">
+        <p class="sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4""> contact@ecoride.fr</p>
+        <div class=" inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <p> Mentions Légales</p>
+    </div>
     </div>
 </footer>
 
